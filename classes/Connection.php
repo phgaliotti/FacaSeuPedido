04baed1ad4914b4276e13f6bb1ddeabeaa4c35cd
Phelipe Galiotti <phelipe.galiotti@outlook.com>
2016-04-10 21:05:55 -0300

    Ajustes

--- classes/Connection.php
+++ classes/Connection.php
@@ -10,7 +10,6 @@ class Connection {
     public static function getInstance() {
 
         if (!isset(self::$instance)) {
-    //self::$instance = new PDO('mysql:host=projetoxdev.mysql.bdaas.com.br;dbname=projetoxdev;charset=utf8', 'projetoxdev', 'projetox123');
             self::$instance = new PDO('mysql:dbname=pecaja;host=localhost;port=21;charset=utf8', 'root', 'abc123');
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
